bc33a451707506e1139efbc2baaeac2bae45aac6
Dev <cdebanjan55@gmail.com>
2021-10-22 14:22:11 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.0.2",
     author="c17hawke",
     description="A small package for dvc ml pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/dvc-ML-demo-AIOps",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/DebanjanCoder/dvc-ML-demo-AIOps",
+    author_email="cdebanjan55@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
